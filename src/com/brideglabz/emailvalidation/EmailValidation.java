48951521f7d772d8caaff8e34fa120d3fbe6ee4d
usha-12 <usha12ahirwar@gmail.com>
2023-02-08 12:56:32 +0530

    Added: check Country code

--- src/com/brideglabz/emailvalidation/EmailValidation.java
+++ src/com/brideglabz/emailvalidation/EmailValidation.java
@@ -7,11 +7,10 @@ public class EmailValidation {
     public static void main(String[] args) {
         System.out.println("Welcome to Regular Expressions program to validate Email IDs!\\n");
         EmailValidation check = new EmailValidation();
-
         String email = "abc.xyz@gmail.com";
         String email1 = "abc____100z@google.com";
-        String email2 = "1abc@bridge-lab.com";
-        String email3 = "_abc@bridgelab.com";
+        String email2 = "1-abc@bridgelab.co.in";
+        String email3 = "abc@bridgelab.co.spain";
         check.validateEmail(email);
         check.validateEmail(email1);
         check.validateEmail(email2);
@@ -19,12 +18,9 @@ public class EmailValidation {
     }
 
     public void validateEmail(String input) {
-
-        String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}$";
-
+        String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
         Pattern pattern = Pattern.compile(email);
         Matcher match = pattern.matcher(input);
-
         printResult(match.matches(), input);
     }
 
