fd0533352f3e0b86e80d928d3f6f98c28df06936
usha-12 <usha12ahirwar@gmail.com>
2023-02-08 12:59:12 +0530

    Added: check all test case

--- src/com/brideglabz/emailvalidation/EmailValidation.java
+++ src/com/brideglabz/emailvalidation/EmailValidation.java
@@ -7,16 +7,32 @@ public class EmailValidation {
     public static void main(String[] args) {
         System.out.println("Welcome to Regular Expressions program to validate Email IDs!\\n");
         EmailValidation check = new EmailValidation();
-        String email = "abc.xyz@gmail.com";
-        String email1 = "abc____100z@google.com";
-        String email2 = "1-abc@bridgelab.co.in";
-        String email3 = "abc@bridgelab.co.spain";
-        check.validateEmail(email);
-        check.validateEmail(email1);
-        check.validateEmail(email2);
-        check.validateEmail(email3);
+        check.testMails();
     }
-
+    public void testMails() {
+        validateEmail("abc@yahoo.com");
+        validateEmail("abc");
+        validateEmail("abc-100@yahoo.com");
+        validateEmail("abc@.com.my");
+        validateEmail("abc.100@yahoo.com");
+        validateEmail("abc123@gmail.a");
+        validateEmail("abc111@abc.com");
+        validateEmail("abc123@.com");
+        validateEmail("abc-100@abc.net");
+        validateEmail("abc123@.com.com");
+        validateEmail("abc.100@abc.com.au");
+        validateEmail(".abc@abc.com");
+        validateEmail("abc@1.com");
+        validateEmail("abc()*@gmail.com");
+        validateEmail("abc@gmail.com.com");
+        validateEmail("abc@%*.com");
+        validateEmail("abc+100@gmail.com");
+        validateEmail("abc..2002@gmail.com");
+        validateEmail("abc.@gmail.com");
+        validateEmail("abc@abc@gmail.com");
+        validateEmail("abc@gmail.com.1a");
+        validateEmail("abc@gmail.com.aa.au");
+    }
     public void validateEmail(String input) {
         String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
         Pattern pattern = Pattern.compile(email);
